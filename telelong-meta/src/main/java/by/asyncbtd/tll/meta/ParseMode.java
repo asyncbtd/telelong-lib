dc9f9bdfda23ac8d47cb64dc0b198556dcd4b16c
Timofey Brukhanchik <asyncbtd@gmail.com>
2025-01-11 16:46:00 +0300

    add object ParseMode

---
+++ telelong-meta/src/main/java/by/asyncbtd/tll/meta/ParseMode.java
@@ -0,0 +1,8 @@
+package by.asyncbtd.tll.meta;
+
+public class ParseMode {
+
+    public static final String MARKDOWN = "Markdown";
+    public static final String MARKDOWNV2 = "MarkdownV2";
+    public static final String HTML = "html";
+}
