ef4d3c48601fefe2587f50b21a6fe3d3318a4d45
Timofey Brukhanchik <asyncbtd@gmail.com>
2025-01-15 15:32:14 +0300

    add serelization and deserelization for InlineKeyboardMarkup

--- telelong-meta/src/main/java/by/asyncbtd/tll/meta/objects/keyboard/inline/InlineKeyboardMarkup.java
+++ telelong-meta/src/main/java/by/asyncbtd/tll/meta/objects/keyboard/inline/InlineKeyboardMarkup.java
@@ -1,9 +1,14 @@
 package by.asyncbtd.tll.meta.objects.keyboard.inline;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class InlineKeyboardMarkup {
 
+    @JsonProperty("inline_keyboard")
     private List<List<InlineKeyboardButton>> inlineKeyboard;
 
     public List<List<InlineKeyboardButton>> getInlineKeyboard() {
