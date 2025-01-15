f08f3359b5eeaaf2f52d1be9c5eea689432a9b92
Timofey Brukhanchik <asyncbtd@gmail.com>
2025-01-15 15:06:43 +0300

    add serelization and deserelization for ChatAdministratorRights

--- telelong-meta/src/main/java/by/asyncbtd/tll/meta/objects/chat/ChatAdministratorRights.java
+++ telelong-meta/src/main/java/by/asyncbtd/tll/meta/objects/chat/ChatAdministratorRights.java
@@ -1,21 +1,54 @@
 package by.asyncbtd.tll.meta.objects.chat;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ChatAdministratorRights {
 
+    @JsonProperty("is_anonymous")
     private Boolean isAnonymous;
+
+    @JsonProperty("can_manage_chat")
     private Boolean canManageChat;
+
+    @JsonProperty("can_delete_messages")
     private Boolean canDeleteMessages;
+
+    @JsonProperty("can_manage_video_chats")
     private Boolean canManageVideoChats;
+
+    @JsonProperty("can_restrict_members")
     private Boolean canRestrictMembers;
+
+    @JsonProperty("can_promote_members")
     private Boolean canPromoteMembers;
+
+    @JsonProperty("can_change_info")
     private Boolean canChangeInfo;
+
+    @JsonProperty("can_invite_users")
     private Boolean canInviteUsers;
+
+    @JsonProperty("can_post_stories")
     private Boolean canPostStories;
+
+    @JsonProperty("can_edit_stories")
     private Boolean canEditStories;
+
+    @JsonProperty("can_delete_stories")
     private Boolean canDeleteStories;
+
+    @JsonProperty("can_post_messages")
     private Boolean canPostMessages;
+
+    @JsonProperty("can_edit_messages")
     private Boolean canEditMessages;
+
+    @JsonProperty("can_pin_messages")
     private Boolean canPinMessages;
+
+    @JsonProperty("can_manage_topics")
     private Boolean canManageTopics;
 
     public Boolean getIsAnonymous() {
