12186ece5773d2ea1071d17914850831d78fd5bc
Timofey Brukhanchik <asyncbtd@gmail.com>
2025-01-15 15:11:20 +0300

    add serelization and deserelization for KeyboardButtonPollType

--- telelong-meta/src/main/java/by/asyncbtd/tll/meta/objects/keyboard/KeyboardButtonPollType.java
+++ telelong-meta/src/main/java/by/asyncbtd/tll/meta/objects/keyboard/KeyboardButtonPollType.java
@@ -1,7 +1,12 @@
 package by.asyncbtd.tll.meta.objects.keyboard;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class KeyboardButtonPollType {
 
+    @JsonProperty("type")
     private String type;
 
     public String getType() {
