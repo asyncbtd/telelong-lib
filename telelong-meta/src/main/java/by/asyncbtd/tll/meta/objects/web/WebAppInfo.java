04b8b70346318f508a7988ce79b7a1a6f77fb0a1
Timofey Brukhanchik <asyncbtd@gmail.com>
2025-01-15 15:13:40 +0300

    add serelization and deserelization for WebAppInfo

--- telelong-meta/src/main/java/by/asyncbtd/tll/meta/objects/web/WebAppInfo.java
+++ telelong-meta/src/main/java/by/asyncbtd/tll/meta/objects/web/WebAppInfo.java
@@ -1,7 +1,12 @@
 package by.asyncbtd.tll.meta.objects.web;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class WebAppInfo {
 
+    @JsonProperty("url")
     private String url;
 
     public String getUrl() {
